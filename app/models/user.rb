1f9d8f9db6c8cde849864d51d629128289d7ac72
Ivan Arakcheev <wildfiler0@gmail.com>
2014-02-14 13:29:56 +0200

    Fix some bugs in registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,18 @@ class User < ActiveRecord::Base
 
   validates :role, presence: true, inclusion: {in: ROLES}
   validates :email, presence: true
+  validates :name, presence: true
 
   has_many :tasks, foreign_key: :owner_id
+  has_and_belongs_to_many :projects  
 
   ROLES.each do |role|
     define_method "is_#{role}?" do
       self.role == role
     end
+  end
+
+  def own_projects
+    self.projects.where(projects:{owner_id: self.id})
   end
 end
