54e75b8d6fb6917bce3f58935ead101051ec109a
Ivan Arakcheev <wildfiler0@gmail.com>
2014-04-13 09:21:30 +0300

    Revert "Remove unused helper methods"
    This reverts commit da806eebb7a8895b49247a0402c114f3812c409a.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def bootstrap_alert_type_for(flash_type)
+    case flash_type
+      when :success
+        "success"
+      when :error
+        "danger"
+      when :alert
+        "warning"
+      when :notice
+        "info"
+      else
+        flash_type.to_s
+    end
+  end
+
+  def flash_message(flash)
+    return nil if flash.empty?
+    flash.map do |type, message|
+      {
+        message: { text: message },
+        type: bootstrap_alert_type_for(type)
+      }
+    end
+  end
 end
